41deac5de464c124c4958bedce212e5dfc04c76f
App Academy Student <student@appacademy.io>
2015-08-06 13:34:15 -0700

    feeds now fetch entries

--- skeleton/app/assets/javascripts/models/feed.js
+++ skeleton/app/assets/javascripts/models/feed.js
@@ -1,6 +1,13 @@
 NewsReader.Models.Feed = Backbone.Model.extend({
   urlRoot: "/api/feeds",
   entries: function() {
-    return new NewsReader.Collections.Entries( {feed:this} );
+    this._entries = this._entries || new NewsReader.Collections.Entries( [], {feed:this} );
+    return this._entries;
+  },
+
+  parse: function(payload) {
+    this.entries().set(payload.latest_entries);
+    delete payload.latest_entries;
+    return payload;
   }
 })
